a7fa84f619026ea627d41040cafce5fb98510358
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-11-30 09:04:08 +0200

    adds subplot and optimizes the quality of the plot

--- code/plot/image.py
+++ code/plot/image.py
@@ -14,6 +14,10 @@ def CreatePlotLine2(S,E,label):
 	plt.plot(S,E,'-r',label=f'{label}')
 	
 
+def GenerateEnergies(spin):
+	return Energy(spin,rd.uniform(1.0,2.0))
+
+
 spinvalues=np.arange(8.5,48.5,2.0)
 avalues=1.69*np.ones(len(spinvalues))
 avalues_th=1.67*np.ones(len(spinvalues))
@@ -25,9 +29,16 @@ envalues=list(energies)
 envalues=list(map(lambda x: x+ rd.randint(1000),envalues))
 envalues_th=list(energies_th)
 
-plt.xlabel(r'I $[\hbar]$')
-plt.ylabel(r'E [a.u.]')
-CreatePlotLine(spinvalues,envalues,f'{avalues[0]}-exp')
-CreatePlotLine2(spinvalues,envalues_th,f'{avalues[0]}-th')
+fig, axes=plt.subplots(2,2,figsize=(10,10),dpi=300)
+#plt.xlabel(r'I $[\hbar]$')
+#plt.ylabel(r'E [a.u.]'
+for ax in axes.flat:
+    ax.set(xlabel='x-label', ylabel='y-label')
+axes[0,0].scatter(spinvalues,list(map(GenerateEnergies,spinvalues)),label='rd-test-1')
+axes[0,1].scatter(spinvalues,list(map(GenerateEnergies,spinvalues)),label='rd-test-2')
+axes[1,0].scatter(spinvalues,list(map(GenerateEnergies,spinvalues)),label='rd-test-3')
+axes[1,1].scatter(spinvalues,list(map(GenerateEnergies,spinvalues)),label='rd-test-4')
+#CreatePlotLine(spinvalues,envalues,f'{avalues[0]}-exp')
+#CreatePlotLine2(spinvalues,envalues_th,f'{avalues[0]}-th')
 plt.legend()
 plt.savefig('plot.pdf',dpi=300,bbox_inches='tight')
