421a820c6326adf54397412f0e06deaf448649e6
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-02 19:52:57 +0200

    adds errorbar plot
    uses plt.errorbar to generate a plot with error bars. uses numpy's random package for generating arrays with random floats

---
+++ code/plot/error_plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from numpy import random as rd
+import numpy as np
+
+xs=np.linspace(0,10,100)
+
+
+def my_f(arg):
+	if(not arg):
+		arg=1
+	return arg+np.log(arg)+np.exp(np.power(arg,0.5))
+
+ys=list(map(my_f,xs))
+
+y2=[x for x in range(10,100)]
+y3=rd.uniform(10,100, size=(1,10))
+
+
+y4=list(map(lambda x: rd.uniform(10,100)*x,xs))
+
+plt.errorbar(xs,ys,yerr=y4)
+plt.savefig("errorplot.pdf",bbox_inches='tight')
