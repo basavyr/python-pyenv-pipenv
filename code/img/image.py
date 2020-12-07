5ced052d63ca1fb70a6ebb4d1bfcdf1be05033d4
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-07 13:00:22 +0200

    adds a single pixel line (with a defined horizontal random chunk of color)

--- code/img/image.py
+++ code/img/image.py
@@ -8,34 +8,31 @@ def GeneratePixel():
 	pixel=[rd.randint(255), rd.randint(255),rd.randint(255)]
 	return pixel
 
-def GenerateListOfPixels(pixel_number,chunks):
+def GenerateLineOfPixels(pixel_number,chunks):
 	pixels=[GeneratePixel() for _ in range(chunks)]
+	print(f'The program generated {chunks} pixels')
+	for pixel in pixels:
+		print(f'p={pixel}')
 	pixel_window=int(pixel_number/chunks)
-#	print(pixel_window)
+	print(f'There will be {chunks} colors that spread across the width {pixel_number}, each with a sub_width of {pixel_window}')
 	pixel_list=[]
 	for pixel in pixels:
-		current_list=[pixel for _ in range(pixel_window)]
-		pixel_list.extend(current_list)
+		for _ in range(pixel_window):
+			pixel_list.extend(pixel)
 	return pixel_list
 
 #the number of chunks represents how many random colors will be generated for a single pixel line (e.g. if n.o. chunks is 3, then the image will have 3 rows of random colors when full image is generated
 chunks=10
-list_of_pixels=GenerateListOfPixels(resolution[1],chunks)
+list_of_pixels=GenerateLineOfPixels(1920,chunks)
 
-print(list_of_pixels)
 
-def GeneratePixelArray(pixel_list,width,height):
+def GenerateImage(pixel_list,width,height):
 	image=[]
-	for id in range(len(pixel_list)):
-		image.extend(pixel for pixel in pixel_list[id])
+	image.append(pixel_list)
 	return image
 
 
-
-pixel_line=GeneratePixelArray(list_of_pixels,resolution[0],resolution[1])
-
-print(pixel_line,len(pixel_line))
-#bg=[pixel_line for _ in range(resolution[1])]
-#print(pixel_line)
-#image=png.from_array(bg,'RGB')
-#image.save('my_image.png')
+pixel_line=GenerateImage(list_of_pixels,10,10)
+ 
+image=png.from_array(pixel_line,'RGB')
+image.save('my_image.png')
