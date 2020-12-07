91efa1fcd0e86339347ac716bc3834387bfac43c
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-07 12:15:03 +0200

    adds image generation with user-defined resolution and fixed chunks

--- code/img/image.py
+++ code/img/image.py
@@ -1,15 +1,35 @@
+import numpy as np
 import png
+from numpy import random as rd
 
 resolution=[1920,1080]
 
-color=(255,0,0)
-pixel1=(100,128,128)
-pixel2=(200,128,75)
-pixels=[]
-[pixels.extend(value for value in pixel1) for _ in range(resolution[0])]
-#print(pixels,len(pixels))
-list_of_pixels=[]
-for _ in range(resolution[1]):
-	list_of_pixels.append(pixels)
-image=png.from_array(list_of_pixels,'RGB')
+def GeneratePixel():
+	pixel=[rd.randint(255), rd.randint(255),rd.randint(255)]
+	return pixel
+
+def GenerateListOfPixels(pixel_number,chunks):
+	pixels=[GeneratePixel() for _ in range(chunks)]
+	pixel_window=int(pixel_number/chunks)
+#	print(pixel_window)
+	pixel_list=[]
+	for pixel in pixels:
+		current_list=[pixel for _ in range(pixel_window)]
+		pixel_list.extend(current_list)
+	return pixel_list
+
+#the number of chunks represents how many random colors will be generated for a single pixel line (e.g. if n.o. chunks is 3, then the image will have 3 rows of random colors when full image is generated
+chunks=10
+list_of_pixels=GenerateListOfPixels(resolution[1],chunks)
+
+def GenerateImage(pixel_list,width,height):
+	image=[]
+	for id in range(len(pixel_list)):
+		image.extend(pixel for pixel in pixel_list[id])
+	return image
+
+pixel_line=GenerateImage(list_of_pixels,resolution[0],resolution[1])
+bg=[pixel_line for _ in range(resolution[1])]
+#print(pixel_line)
+image=png.from_array(bg,'RGB')
 image.save('my_image.png')
