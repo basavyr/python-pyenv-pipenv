50a427962bd4e8601325e0a27b083d038136ffd0
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-20 19:55:00 +0200

    generates constant background from pixel and resolution

--- code/collage-maker/matrix.py
+++ code/collage-maker/matrix.py
@@ -1,33 +1,52 @@
 import numpy as np
+import png
+from numpy import random as rd
 
+
+out_file='./graphs_output/'
 # Resolution
-width, height = 1920, 1080
+width, height = 30, 20
 
-resolution=f'Screen res: {width}x{height} px'
-
-bg = np.array([[1, 2, 3], [3, 2, 1]])
+resolution = f'Screen res: {width}x{height} px'
 
 print(resolution)
 
-def GenerateMatrix(width, height):
-    l=np.linspace(1,100,width)
-    ret_m=np.array([l])
-    for _ in range(height-1):
-        line=np.array(l)
-        ret_m=np.vstack([ret_m,line])
-    return ret_m
-    
+def GenerateBG(width,height):
+    # Steelblue color (testing)
+    pixel=[70,130,180]
+    bg=[]
+    line_of_pixels=[]
+    for _ in range(width):
+        line_of_pixels.extend(pixel)
+    for _ in range(height):
+        bg.append(line_of_pixels)
+    return bg
+
+bg=GenerateBG(1920,1080)
+image=png.from_array(bg,'RGB')
+image.save(f'{out_file}testBG.png')
+
+# def GenerateMatrix(width, height):
+#     l = np.linspace(1, 100, width)
+#     ret_m = np.array([l])
+#     for _ in range(height-1):
+#         line = np.array(l)
+#         ret_m = np.vstack([ret_m, line])
+#     return ret_m
 
 
-def ShowMatrix(mat):
-    id=1
-    for row in mat:
-        print(f'Row id={id}: {row}')
-        id=id+1
+# def ShowMatrix(mat):
+#     id = 1
+#     for row in mat:
+#         print(f'Row id={id}: {row}')
+#         id = id+1
 
-m=GenerateMatrix(width,height)
-print(np.size(m,axis=0))
 
-# ShowMatrix(bg)
+# m = GenerateMatrix(width, height)
 
-# print(np.vstack([bg,[1,2,2]]))
+# print(f'Size of M: {np.size(m,axis=1)}x{np.size(m,axis=0)} pixels')
+
+
+# # ShowMatrix(bg)
+
+# # print(np.vstack([bg,[1,2,2]]))
