7d948b3102552ddd7360f08321c1a45b88d1d274
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-22 08:17:11 +0200

    fixes the subprocesses routines. adds conditional compilation.

--- code/copy-file-adjust/copy-file.py
+++ code/copy-file-adjust/copy-file.py
@@ -1,43 +1,40 @@
+#!/Users/robertpoenaru/.pyenv/shims/python
 import os
 import subprocess
-
-path = './latex-content/'
-
-tex_file = open(f'{path}draft.tex', 'r')
-
-tex_copy = open(f'{path}preprint.tex', 'w+')
 
 # create a preprint mode from the original paper draft
 count = 1
 # special_command='% !TEX options=--shell-escape\n'
 # tex_copy.write(special_command)
-for line in tex_file.readlines():
-    if(count == 2):
-        line = 'p'+line[1:]
-    tex_copy.write(line)
-    count = count+1
+# for line in tex_file.readlines():
+#     if(count == 2):
+#         line = 'p'+line[1:]
+#     tex_copy.write(line)
+#     count = count+1
 
-print('...Finished generating the preprint...')
+draft_mode = False
 
-compile_draft = 'pdflatex ./latex-content/draft.tex'
-compile_preprint = 'pdflatex -shell-escape  ./latex-content/preprint.tex'
+if(draft_mode):
+    print('Building the latex file [draft-mode]...')
+    p1 = subprocess.run(['pdflatex', '-shell-escape',
+                         './latex-content/my_prc.tex'])
+    print('Cleaning-up the remaining files...')
+    os.system('rm *.log')
+    os.system('rm *.aux')
+    os.system('rm *.bib')
 
-# os.system(compile_draft)
-# # Clean content
-# os.system('rm *.log')
-# os.system('rm *.aux')
-# os.system('rm *.bib')
+preprint_mode = False
+if(preprint_mode):
+    print('Building the latex file [preprint-mode]...')
+    p1 = subprocess.run(['pdflatex', '-shell-escape',
+                         './latex-content/my_prc2.tex'])
+    print('Cleaning-up the remaining files...')
+    os.system('rm *.log')
+    os.system('rm *.aux')
+    os.system('rm *.bib')
 
-# os.system(f'pdflatex -shell-escape -interaction=nonstopmode ./latex-content/preprint.tex')
 
-# Clean content
-# subprocess.run(['pdflatex','-shell-escape ./latex-content/preprint.tex'],shell=True)
-# subprocess.run(['pdflatex','-shell-escape','-interaction=nonstopmode','./latex-content/draft.tex']) 
+clean_pdf_files = False
 
-# subprocess.run(['cp','./latex-content/preprint.tex', 'preprint.tex']) 
-
-subprocess.run(['pdflatex','-interaction=nonstopmode','-shell-escape','preprint.tex']) 
-
-# os.system('rm *.log')
-# os.system('rm *.aux')
-# os.system('rm *.bib')
+if(clean_pdf_files):
+    clean = subprocess.run(['rm', '*.pdf'])
