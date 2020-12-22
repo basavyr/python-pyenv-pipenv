f57e9496cf93f0f5f3280fc58dde0d5592c02b0b
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-22 06:55:11 +0200

    implements shell scripting with `subprocesses`
    issues with latex compilation still persists

--- code/copy-file-adjust/copy-file.py
+++ code/copy-file-adjust/copy-file.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 path = './latex-content/'
 
@@ -8,6 +9,8 @@ tex_copy = open(f'{path}preprint.tex', 'w+')
 
 # create a preprint mode from the original paper draft
 count = 1
+# special_command='% !TEX options=--shell-escape\n'
+# tex_copy.write(special_command)
 for line in tex_file.readlines():
     if(count == 2):
         line = 'p'+line[1:]
@@ -17,7 +20,7 @@ for line in tex_file.readlines():
 print('...Finished generating the preprint...')
 
 compile_draft = 'pdflatex ./latex-content/draft.tex'
-compile_preprint = 'sudo pdflatex ./latex-content/preprint.tex'
+compile_preprint = 'pdflatex -shell-escape  ./latex-content/preprint.tex'
 
 # os.system(compile_draft)
 # # Clean content
@@ -25,8 +28,16 @@ compile_preprint = 'sudo pdflatex ./latex-content/preprint.tex'
 # os.system('rm *.aux')
 # os.system('rm *.bib')
 
-os.system(compile_preprint)
+# os.system(f'pdflatex -shell-escape -interaction=nonstopmode ./latex-content/preprint.tex')
+
 # Clean content
-os.system('rm *.log')
-os.system('rm *.aux')
-os.system('rm *.bib')
+# subprocess.run(['pdflatex','-shell-escape ./latex-content/preprint.tex'],shell=True)
+# subprocess.run(['pdflatex','-shell-escape','-interaction=nonstopmode','./latex-content/draft.tex']) 
+
+# subprocess.run(['cp','./latex-content/preprint.tex', 'preprint.tex']) 
+
+subprocess.run(['pdflatex','-interaction=nonstopmode','-shell-escape','preprint.tex']) 
+
+# os.system('rm *.log')
+# os.system('rm *.aux')
+# os.system('rm *.bib')
