b792fb0227411b512f9b90f3c9fecf07d371538f
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-22 13:38:59 +0200

    compiles the latex file in draft mode.

--- code/copy-file-adjust/copy-file.py
+++ code/copy-file-adjust/copy-file.py
@@ -3,7 +3,6 @@ import os
 import subprocess
 
 # create a preprint mode from the original paper draft
-count = 1
 # special_command='% !TEX options=--shell-escape\n'
 # tex_copy.write(special_command)
 # for line in tex_file.readlines():
@@ -26,15 +25,23 @@ if(draft_mode):
 preprint_mode = False
 if(preprint_mode):
     print('Building the latex file [preprint-mode]...')
-    p1 = subprocess.run(['pdflatex', '-shell-escape',
-                         './latex-content/my_prc2.tex'])
-    print('Cleaning-up the remaining files...')
-    os.system('rm *.log')
-    os.system('rm *.aux')
-    os.system('rm *.bib')
+    try:
+        subprocess.check_call(['pdflatex', '-shell-escape', '-interaction=nonstopmode',
+                               './latex-content/my_prc2.tex'], capture_output=True, text=True)
+    except:
+        print('Latex compilation failed...')
+
+    # print('Cleaning-up the remaining files...')
+    # os.system('rm *.log')
+    # os.system('rm *.aux')
+    # os.system('rm *.bib')
+    # print(p1.stderr.decode())
 
 
-clean_pdf_files = False
+clean_pdf_files = True
 
 if(clean_pdf_files):
-    clean = subprocess.run(['rm', '*.pdf'])
+    try:
+        subprocess.check_call(['rm', '*.pdf'], capture_output=True, text=True)
+    except:
+        print('failed to delete')
