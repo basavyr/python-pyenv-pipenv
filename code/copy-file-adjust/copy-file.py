3547b7816d2c0907de3f433f407076a8dea766cc
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-23 20:43:06 +0200

    modifies the preprint file directly in a line by line writing procedure

--- code/copy-file-adjust/copy-file.py
+++ code/copy-file-adjust/copy-file.py
@@ -2,11 +2,30 @@
 import os
 import subprocess
 
+# create a copy of the file
 
-clean_pdf_files = True
+# file_name = subprocess.run(
+#     ['cp', './latex-content/my_prc.tex', './latex-content/my_prc_preprint.tex'], stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+
+
+paper_draft_mode = False
+
+
+with open('./latex-content/my_prc.tex', 'rt') as draft:
+    preprint = open('./latex-content/my_prc_preprint.tex', 'wt')
+    lines = draft.readlines()
+    count = 1
+    for line in lines:
+        if(count == 2):
+            newline = 'preprint,\n'
+            preprint.write(newline)
+        else:
+            preprint.write(line)
+        count = count+1
+
 
 # draft mode
-if(clean_pdf_files):
+if(paper_draft_mode):
     try:
         # f=open("out.dat","w+")
         # subprocess.check_output(
@@ -22,16 +41,16 @@ if(clean_pdf_files):
         print(out.decode())
 
 
-clean_pdf_files = True
+paper_preprint_mode = False
 
 # preprint mode
-if(clean_pdf_files):
+if(paper_preprint_mode):
     try:
         # f=open("out.dat","w+")
         # subprocess.check_output(
         #     ['rm', '*.pdf'], stderr=subprocess.PIPE)
         proc = subprocess.Popen(
-            ['pdflatex', './latex-content/my_prc2.tex'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            ['pdflatex', './latex-content/my_prc_preprint.tex'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         out, err = proc.communicate()
     # ['ls', './dir'], capture_output=True, text=True, shell=True)
     except subprocess.CalledProcessError:
@@ -40,7 +59,25 @@ if(clean_pdf_files):
     else:
         print(out.decode())
 
-os.system('rm *.bib')
-os.system('rm *.log')
-os.system('rm *.aux')
-# os.system('rm *.dat')
+# try:
+#     p = os.system('rm *.bib')
+# except p.OSError as e:
+#     print(e)
+
+# try:
+#     os.system('rm *.aux')
+# except OSError as e:
+#     print(e)
+
+# try:
+#     os.system('rm *.log')
+# except OSError as e:
+#     print(1)
+
+abs_clean = False
+
+if (abs_clean == True):
+    try:
+        os.system('rm *.pdf')
+    except OSError as e:
+        print(1)
