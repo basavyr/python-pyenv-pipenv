db5114410fbb1158df28196ee2588232a13cba7f
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-23 20:02:46 +0200

    compiles with pdflatex (python) both files

--- code/copy-file-adjust/copy-file.py
+++ code/copy-file-adjust/copy-file.py
@@ -40,11 +40,42 @@ if(preprint_mode):
 
 clean_pdf_files = True
 
+# draft mode
 if(clean_pdf_files):
     try:
         # f=open("out.dat","w+")
-        subprocess.check_output(
-            ['touch', 'touched.dat'], stderr=subprocess.PIPE)
+        # subprocess.check_output(
+        #     ['rm', '*.pdf'], stderr=subprocess.PIPE)
+        proc = subprocess.Popen(
+            ['pdflatex', './latex-content/my_prc.tex'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        out, err = proc.communicate()
     # ['ls', './dir'], capture_output=True, text=True, shell=True)
-    except subprocess.CalledProcessError as e:
-        print(e.stderr.decode())
+    except subprocess.CalledProcessError:
+        print(err.decode())
+        # print(e.stderr.decode())
+    else:
+        print(out.decode())
+
+
+clean_pdf_files = True
+
+# preprint mode
+if(clean_pdf_files):
+    try:
+        # f=open("out.dat","w+")
+        # subprocess.check_output(
+        #     ['rm', '*.pdf'], stderr=subprocess.PIPE)
+        proc = subprocess.Popen(
+            ['pdflatex', './latex-content/my_prc2.tex'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+    # ['ls', './dir'], capture_output=True, text=True, shell=True)
+    except subprocess.CalledProcessError:
+        print(err.decode())
+        # print(e.stderr.decode())
+    else:
+        print(out.decode())
+
+os.system('rm *.bib')
+os.system('rm *.log')
+os.system('rm *.aux')
+# os.system('rm *.dat')
