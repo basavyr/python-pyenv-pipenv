a12c7785543df41fb55f71f0d5bced64ccccd788
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-05 06:49:06 +0300

    optimized the citation reader for full-latex files. saves citations to external source

--- code/citation-extractor/extract.py
+++ code/citation-extractor/extract.py
@@ -16,20 +16,41 @@ def Get_Line_Citations(LINE):
 
 
 def Create_Reference_File(input_file, ref_file):
+
     REFERENCES = []
+
+    # failsafe
+    # OK[0] -> +1 AFTER CREATING THE LIST OF REFS
+    # OK[1] -> +1 AFTER WRITING THE LIST OF REFS TO EXTERNAL FILE
+    OK = [1, 1]
+
+    # reading the latex file
+    # extracting each citation line-by-line
+    # append individual reference to a list
     with open(input_file, 'r+') as reader:
         latex_file = reader.readlines()
+        read_count = 0
         for latex_line in latex_file:
             line = latex_line.strip()
             line_cites = Get_Line_Citations(line)
             # print(line_cites)
             for cite in line_cites:
                 REFERENCES.append(cite)
+            read_count += 1
+        if(read_count != len(latex_file)):
+            OK[0] = 0
+
+    # save each reference from the list into an external file
     with open(ref_file, 'w+') as writer:
+        write_count = 0
         for ref in REFERENCES:
             writer.writelines(ref)
             writer.writelines('\n')
+            write_count += 1
+        if(write_count != len(REFERENCES)):
+            OK[1] = 0
 
+    print(OK)
     return REFERENCES
 
 
