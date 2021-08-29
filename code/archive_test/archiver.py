208f6308332ef9e59dd0543fba5ad76511d5e6df
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-08-29 09:08:43 +0300

    add skeleton functions for listing only dirs and files

--- code/archive_test/archiver.py
+++ code/archive_test/archiver.py
@@ -4,7 +4,21 @@ import subprocess
 # The directory in which all the files that require compression are placed into
 content_directory = os.getcwd()
 
-for meta_file in os.listdir(content_directory):
-    current_path = meta_file
-    if(os.path.isdir(current_path)):
-        print(current_path)
+
+def ListDirectories(current_path):
+    meta_files = [x for x in os.listdir(current_path)]
+    if(len(meta_files)):
+        return meta_files
+    else:
+        return 'Dirs', -1
+
+
+def ListFiles(current_path):
+    meta_files = [x for x in os.listdir(current_path)]
+    if(len(meta_files)):
+        return meta_files
+    else:
+        return 'Files', -1
+
+print(ListDirectories(content_directory))
+print(ListFiles(content_directory))
