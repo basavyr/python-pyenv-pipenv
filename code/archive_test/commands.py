d9d38363cd0b39e2a25d0de62ec0d7b3a18a0013
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-08-29 15:41:34 +0000

    update the command list with proper return code

--- code/archive_test/commands.py
+++ code/archive_test/commands.py
@@ -3,5 +3,7 @@ import subprocess
 
 def RunCommand(command_name):
     cmd = command_name
-    executed_cmd = subprocess.run(["zip", "-v"])
-    print(executed_cmd.stdout)
+    executed_cmd = subprocess.run(["ls"])
+    if(executed_cmd.returncode == 0):
+        print('Command executed succesfully')
+        # print(executed_cmd)
