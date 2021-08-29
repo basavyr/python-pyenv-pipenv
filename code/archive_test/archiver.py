0385faa890b1138380d3a98a0518f4b46a0ddb5c
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-08-29 09:05:34 +0300

    only prints the folders within the root directory

--- code/archive_test/archiver.py
+++ code/archive_test/archiver.py
@@ -1,3 +1,10 @@
 import os
+import subprocess
 
+# The directory in which all the files that require compression are placed into
+content_directory = os.getcwd()
 
+for meta_file in os.listdir(content_directory):
+    current_path = meta_file
+    if(os.path.isdir(current_path)):
+        print(current_path)
