24ed7ae697a349df1d0507d6435330c855a15809
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-09-03 12:46:52 +0300

    proper cat for the gem  into pre-configured location

--- code/archive_test/unarchiver.py
+++ code/archive_test/unarchiver.py
@@ -1,3 +1,4 @@
+from genericpath import isdir
 import os
 import shutil
 import subprocess
@@ -21,14 +22,31 @@ def PackCommand(cmd_name, cmd_args):
 
 def CatProcess(copied_directory, archive_name):
     """
-    define the cat command which packs the zipped chunks into a single file
+    Define the cat command which packs the zipped chunks into a single file.
+    The cat command takes the chunks and creates a single "gem" within a pre-configured location
     """
 
-    cat_cmd = f'cat {archive_name}.z* > {copied_directory}'
+    packed_gem = "unpacked.zip"
+
+    cat_cmd = f'cat {archive_name}.z* > {copied_directory}{packed_gem}'
     cat_xargs = []
+
+    # check if the copied directory exists or not
+    dirs = [x for x in os.listdir(os.getcwd()) if os.path.isdir(x)]
+    if (copied_directory in dirs):
+        pass
+        # print('copied folder exists')
+    else:
+        # print('copied folder does not exist')
+        try:
+            os.mkdir(copied_directory)
+        except FileExistsError:
+            pass
 
     # use a True shell state in order to consider the wildcard * when packing multiple chunks with the same name
     commands.RunCommand(cat_cmd, cat_xargs, True)
 
 
-CatProcess(archiver.copied_directory, archiver.archive_name)
+if __name__ == '__main__':
+    CatProcess(archiver.copied_directory, archiver.archive_name)
+    archiver.CleanArchives(archiver.current_directory, archiver.archive_name)
