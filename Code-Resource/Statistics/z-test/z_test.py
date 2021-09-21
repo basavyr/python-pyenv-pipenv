addd89bc56a9b0980469beab863110046bd4aca5
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-09-21 21:18:34 +0300

    update to the proper rng seed using the default rng function

--- Code-Resource/Statistics/z-test/z_test.py
+++ Code-Resource/Statistics/z-test/z_test.py
@@ -1,17 +1,21 @@
 import numpy as np
 from numpy import random as rd
+
+# according to the documentation, this is the superior way of using the random package within Python [https://numpy.org/doc/stable/reference/random/index.html#random-quick-start]
+from numpy.random import default_rng
 import matplotlib.pyplot as plt
 
-x1 = np.array(rd.standard_normal(100))
+rng = default_rng()
 
-print(x1)
+x1 = np.array(rd.standard_normal(100))
+x2 = rng.standard_normal(100)
 
 x = np.array(rd.normal(0, 0.2, 100))
 
-y = np.linspace(0, 100, len(x))
+y = np.linspace(-1, 1, len(x))
 
-
-plt.plot(y, x1, '-r', label='data')
+plt.plot(y, x1, '-r', label='data1')
+plt.plot(y, x2, '--k', label='data2')
 plt.legend(loc='best')
 plt.savefig('normal_plot.pdf', bbox_inches='tight', dpi=300)
 plt.close()
