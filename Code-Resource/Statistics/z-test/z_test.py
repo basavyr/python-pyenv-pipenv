2d1f6d7fd4d373e897aeb1b4b515bb0e4c574219
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-09-21 22:47:21 +0300

    draws a gaussian curve

--- Code-Resource/Statistics/z-test/z_test.py
+++ Code-Resource/Statistics/z-test/z_test.py
@@ -18,8 +18,16 @@ x = np.array(rd.normal(0, 0.2, 100))
 
 y = np.linspace(-1, 1, len(x))
 
-plt.plot(y, x1, '-r', label='data1')
-plt.plot(y, x2, '--k', label='data2')
-plt.legend(loc='best')
+# plt.plot(y, x1, '-r', label='data1')
+# plt.plot(y, x2, '--k', label='data2')
+# plt.legend(loc='best')
+# plt.savefig('normal_plot.pdf', bbox_inches='tight', dpi=300)
+
+
+#x-axis ranges from -3 and 3 with .001 steps
+x = np.arange(-3, 3, 0.001)
+
+#plot normal distribution with mean 0 and standard deviation 1
+plt.plot(x, norm.pdf(x, 0, 1))
 plt.savefig('normal_plot.pdf', bbox_inches='tight', dpi=300)
 plt.close()
