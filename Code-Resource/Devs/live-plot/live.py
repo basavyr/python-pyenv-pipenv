c4e8d964d5d2b8424ad4eb7e9bbc0e960d0d1ac1
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-08 12:48:19 +0200

    add link for live plot tutorial

--- Code-Resource/Devs/live-plot/live.py
+++ Code-Resource/Devs/live-plot/live.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# Guide for live plot
+# https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Matplotlib/09-LiveData/starting_code.py
+
 from itertools import count
 
 x=count(1)
