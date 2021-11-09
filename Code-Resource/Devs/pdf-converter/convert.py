c62954ad09d0909301c54bb54d54d053b18b5976
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-09 17:19:21 +0200

    proper conversion of a pdf file within multiple folders

--- Code-Resource/Devs/pdf-converter/convert.py
+++ Code-Resource/Devs/pdf-converter/convert.py
@@ -10,20 +10,28 @@ current_path = os.getcwd()
 
 folders = [x for x in os.listdir(current_path) if os.path.isdir(x)]
 
+id = 0
+
 for dir in folders:
     content = os.listdir(dir)
     current_dir_path = os.path.join(
         os.path.relpath(os.getcwd()), os.path.relpath(dir))
     # print(current_dir_path)
 
+    file_type = ".md"
+
     pdf_files = [pdf for pdf in content if ".pdf" in pdf]
+    md_files = [md for md in content if file_type in md]
+
     pdf_file_paths = [os.path.join(
         str(current_dir_path), str(pdf_file)) for pdf_file in pdf_files]
-    for path in pdf_file_paths:
+    md_file_paths = [os.path.join(
+        str(current_dir_path), str(md_file)) for md_file in md_files]
+
+    for path in md_file_paths:
         print(path)
-        # subprocess.run(["python", "convert.py", path], capture_output=False)
-    # for pdf_file in pdf_files:
-    # print(os.path.join(str(current_dir_path), str(pdf_file)))
+        subprocess.run(["pandoc", path, f"-o file{id+1}.pdf"])
+        id += 1
 
 
 def Get_PDF_Files(directory):
