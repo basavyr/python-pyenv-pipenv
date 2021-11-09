4cf2ce753fb08a5eda763dcade614d8f31c8c006
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-09 16:52:16 +0200

    Update convert.py

--- Code-Resource/Devs/pdf-converter/convert.py
+++ Code-Resource/Devs/pdf-converter/convert.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 import subprocess
+from pathlib import Path
+
 
 subprocess.run(["python", "--version"], capture_output=False)
 
@@ -8,9 +10,17 @@ current_path = os.getcwd()
 
 folders = [x for x in os.listdir(current_path) if os.path.isdir(x)]
 
-print(folders)
-
 for dir in folders:
     content = os.listdir(dir)
-    pdf_files = [pdf for pdf in content if ".pdf" in pdf]
-    print(pdf_files)
+    current_dir_path = os.path.abspath(dir)
+    for file in content:
+        print(os.path.join(str(current_dir_path), str(file)))
+
+    # pdf_files = [pdf for pdf in content if ".pdf" in pdf]
+    # for pdf in pdf_files:
+    # print("FilePath:", Path(pdf).parent.absolute())
+    # print(os.path.abspath(pdf))
+
+
+def Get_PDF_Files(directory):
+    print("this is a pdf file")
