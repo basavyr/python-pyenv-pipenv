ab92fe9de89425e6103079f197b0081e26e3aba9
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-09 17:06:39 +0200

    able to print all the pdf files within cd

--- Code-Resource/Devs/pdf-converter/convert.py
+++ Code-Resource/Devs/pdf-converter/convert.py
@@ -14,12 +14,16 @@ for dir in folders:
     content = os.listdir(dir)
     current_dir_path = os.path.join(
         os.path.relpath(os.getcwd()), os.path.relpath(dir))
-    print(current_dir_path)
-    pdf_files = [pdf for pdf in content if ".pdf" in pdf]
+    # print(current_dir_path)
 
+    pdf_files = [pdf for pdf in content if ".pdf" in pdf]
+    pdf_file_paths = [os.path.join(
+        str(current_dir_path), str(pdf_file)) for pdf_file in pdf_files]
+    for path in pdf_file_paths:
+        print(path)
+        # subprocess.run(["python", "convert.py", path], capture_output=False)
     # for pdf_file in pdf_files:
-    #     print(pdf_file)
-    #     print(os.path.join(str(current_dir_path), str(pdf_file)))
+    # print(os.path.join(str(current_dir_path), str(pdf_file)))
 
 
 def Get_PDF_Files(directory):
