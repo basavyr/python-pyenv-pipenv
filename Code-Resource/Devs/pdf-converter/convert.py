a15da5329d16594b11f0838b8d1f1923ccdfd816
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-09 15:15:36 +0200

    create a function that shows all content

--- Code-Resource/Devs/pdf-converter/convert.py
+++ Code-Resource/Devs/pdf-converter/convert.py
@@ -3,3 +3,13 @@ import os
 import subprocess
 
 subprocess.run(["python", "--version"], capture_output=False)
+
+current_path = os.getcwd()
+
+folders = [x for x in os.listdir(current_path) if os.path.isdir(x)]
+
+print(folders)
+
+for dir in folders:
+    content = os.listdir(dir)
+    print(content)
