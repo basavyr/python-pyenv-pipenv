4c15fbc6e527f016fa50e749cfc5e784f24117c2
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-20 19:19:32 +0200

    Update taylor.py

--- Code-Resource/Math/taylor-expansion/taylor.py
+++ Code-Resource/Math/taylor-expansion/taylor.py
@@ -17,7 +17,12 @@ test = Taylor()
 
 xdata = [i for i in range(0, 10)]
 coeff_list = lambda n: [test.c(n, 0, x)for x in xdata]
-plt.plot(xdata, coeff_list(1), '-r', label='test poly')
-plt.plot(xdata, coeff_list(2), '-b', label='test poly')
+
+plt_y = lambda n, lbl: plt.plot(xdata, coeff_list(n), '-b', label=f'c_n={lbl}')
+
+
+plt_y(1, 1)
+plt_y(2, 2)
+plt.legend(loc='best')
 plt.show()
 plt.close()
