dfcc3f61e7c5878f6b59b2d5358de0ff96949f70
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-09 17:16:51 +0200

    Update main.py

--- Code-Resource/Devs/type_checker/main.py
+++ Code-Resource/Devs/type_checker/main.py
@@ -1,16 +1,7 @@
-number = 50
-
-
-char = 'bishu'
-
 
 def giveType(object):
     obj_type = type(object)
     return obj_type
-
-
-# print(giveType(char))
-# print(giveType(number))
 
 
 def checkVarType(var, var_type):
@@ -18,8 +9,9 @@ def checkVarType(var, var_type):
     try:
         assert instance_type is True
     except AssertionError as error:
-        print(f'The variable has a different type that the required one -> {giveType(var)} | {var_type}')
+        # print(
+        #     f'The variable has a different type that the required one -> {giveType(var)} | {var_type}')
+        return -1
     else:
-        print(f'All good -> {giveType(var)} | {var_type}')
-
-checkVarType(number, str)
+        # print(f'All good -> {giveType(var)} | {var_type}')
+        return 1
