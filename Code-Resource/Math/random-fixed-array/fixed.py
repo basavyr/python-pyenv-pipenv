ecc578cfe82fb363055258f889319a428115b537
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-26 09:36:34 +0200

    add function that generates a random array up to a certain value

---
+++ Code-Resource/Math/random-fixed-array/fixed.py
@@ -0,0 +1,49 @@
+from tabnanny import check
+from timeit import default_timer
+import time
+import random
+
+
+_MIN_NUMBER = 5
+_MAX_NUMBER = 35
+
+
+def get_random_number():
+    """ Generates a random number between [5 and 35]"""
+    rd_n = random.randrange(_MIN_NUMBER, _MAX_NUMBER)
+    return rd_n
+
+
+def generate_fixed_array(arr_size):
+    random_array = []
+
+    total_sum = 0
+    checker = True
+
+    # ****************************
+    start = default_timer()
+    # ****************************
+    while(checker):
+        rng = get_random_number()
+        if(total_sum + _MIN_NUMBER > 100):
+            print(f'will stop the rng with sum {total_sum}')
+            checker = False
+        elif(total_sum + rng < 100):
+            print(f'✅ found good number {rng} with current sum to {total_sum}')
+            total_sum = total_sum + rng
+            random_array.append(rng)
+    # ****************************
+    end = default_timer()
+    # ****************************
+
+    proc_time = float(end - start)
+    return random_array, proc_time
+
+
+def main():
+    w = generate_fixed_array(5)[0]
+    print(w)
+
+
+if __name__ == '__main__':
+    main()
