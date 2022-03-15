cc78ee90a6666840248056c6c49a5a196ed4f8f5
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-15 14:10:30 +0200

    stop the thread if even number is generated

--- Code-Resource/Devs/threading_tasks/bg_Task.py
+++ Code-Resource/Devs/threading_tasks/bg_Task.py
@@ -1,5 +1,5 @@
 import threads_main as local_thread
-
+import random
 # do not work with smart thread
 thread = local_thread.threading.Thread()
 # create an event which will turn off a thread
@@ -18,22 +18,29 @@ def create_stop_event(event):
     return event.set()
 
 
+def generate_number():
+    return random.randint(0, 100)
+
+
 def main():
-    # do some calculations
     print('thread is unset')
     while stop_event_thread.is_set() is False:
-        print('...')
         local_thread.time.sleep(3)
+        rng = generate_number()
+        res = 1 if rng % 2 == 0 else 0
+        print(f'Generated {rng} -> {res}')
+        if(res):
+            create_stop_event(stop_event_thread)
+
         # if(check_for_event(stop_event_thread) == 0):
         #     pass
         # else:
         #     create_stop_event(stop_event_thread)
-        create_stop_event(stop_event_thread)
         # try:
         #     assert value is True, 'The thread is not set!'
         # except AssertionError as err:
         #     print(err)
-        print('finished the task execution')
+    print('finished the task execution')
 
 
 if __name__ == '__main__':
