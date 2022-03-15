66cafa45d4416d0c511984dcab6d84e529a0af0d
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-15 10:41:30 +0200

    Update threads_main.py

--- Code-Resource/Devs/threading_tasks/threads_main.py
+++ Code-Resource/Devs/threading_tasks/threads_main.py
@@ -1,34 +1,50 @@
+from concurrent.futures import thread
 import threading
 import time
 
+# source: https://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread/49877671#49877671
+# main discussion that shows a possible solution: https://github.com/miguelgrinberg/Flask-SocketIO/issues/1395
 
-class KillableThread(threading.Thread):
-    def __init__(self, sleep_interval=1):
+
+class SmartThread(threading.Thread):
+    def __init__(self, sleep_interval=1, thread_id=''):
         super().__init__()
-        self._kill = threading.Event()
-        self._interval = sleep_interval
+        self._kill_event = threading.Event()
+        self._sleep_interval = sleep_interval
+        self._thread_id = thread_id
 
     def run(self):
+        ok = True
+        idx = 1
         while True:
-            print("Do Something")
+            print(f'Operation {idx}')
 
             # If no kill signal is set, sleep for the interval,
             # If kill signal comes in while sleeping, immediately
             #  wake up and handle
-            is_killed = self._kill.wait(self._interval)
+            is_killed = self._kill_event.wait(self._sleep_interval)
             if is_killed:
+                ok = False
                 break
+            idx = idx + 1
 
-        print("Killing Thread")
+        print(f"Stopping thread -> {self._thread_id}")
 
     def kill(self):
-        self._kill.set()
+        self._kill_event.set()
 
 
-t = KillableThread()
+def main():
+    test_thread = SmartThread(sleep_interval=1, thread_id='thd01')
+    test_thread.start()
 
-t.start()
+    # this is inside the main function
+    # outside the smart thread class
+    time.sleep(3)
+
+    # kill the background task executed by the smart thread
+    test_thread.kill()
 
 
-time.sleep(5)
-t.kill()
+if __name__ == '__main__':
+    main()
