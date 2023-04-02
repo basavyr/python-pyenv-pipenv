2e3fcfafa282994a1c696b8118a95aa66de8b1f4
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-04-02 09:52:02 +0300

    start the elasticsearch process in the background
    also captures the logs

--- Code-Resource/Devs/elasticsearch/app/src/tools/start_elasticsearch.sh
+++ Code-Resource/Devs/elasticsearch/app/src/tools/start_elasticsearch.sh
@@ -6,6 +6,7 @@ if [ -z "$ES_HOME" ]; then
   echo "The path to Elasticsearch is not available"
 else
   echo "Elasticsearch is configured at: $ES_HOME"
+  echo "Starting Elasticsearch service on $host"
+  cd $ES_HOME
+  ./bin/elasticsearch -d -p elasticsearch.pid > elasticsearch.log 2>&1``
 fi
-
-echo "Starting Elasticsearch service on $host"
