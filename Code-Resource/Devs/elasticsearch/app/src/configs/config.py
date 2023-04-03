4409e3e5194b2192f2ad0342c95e1bdd8a9d1681
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-04-03 18:12:26 +0300

    add api key in config

--- Code-Resource/Devs/elasticsearch/app/src/configs/config.py
+++ Code-Resource/Devs/elasticsearch/app/src/configs/config.py
@@ -3,11 +3,14 @@ import os
 ES_HOME = ''
 if (os.environ['ES_HOME'] != ""):
     ES_HOME = os.environ['ES_HOME']
+    ID = os.environ['ES_API_KEY_ID']
+    KEY = os.environ['ES_API_KEY']
 
 ES_CERT = "http_ca.crt"
 ES_HOST = "https://localhost:9200"
 ES_API_KEY = {
-    "id": "",
-    "key": ""}
+    "id": ID,
+    "key": KEY
+}
 
 CERT_PATH = os.path.join(ES_HOME, 'config/certs', ES_CERT)
