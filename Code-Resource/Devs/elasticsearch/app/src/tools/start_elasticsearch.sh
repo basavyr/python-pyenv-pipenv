ea3cc3a40a10c7eea735412dc3e73810714ae8f7
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-04-03 15:57:13 +0300

    improve the start script

--- Code-Resource/Devs/elasticsearch/app/src/tools/start_elasticsearch.sh
+++ Code-Resource/Devs/elasticsearch/app/src/tools/start_elasticsearch.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env sh
 
-host=`uname -a | awk '{print $4}'`
+#config vars
+MACOSOS=Darwin
+MB_AIR="Roberts-MacBook-Air.local"
+LOCAL_DIR=`pwd`
 
-if [ -z "$ES_HOME" ]; then
-  echo "The path to Elasticsearch is not available"
+# takes the OS version of the current platform on which the script is running
+HOST=`uname -a | awk '{print $4}'`
+
+if [ "$HOST" == $MACOSOS ] || [ "$HOST" == "Darwin" ] || [ "$HOST" == "macOS" ]; then
+  echo "The current platform is MacOS"
+  ELK_MODE="cd $ES_HOME && ./bin/elasticsearch -d -p $LOCAL_DIR/elasticsearch.pid > $LOCAL_DIR/elasticsearch.log 2>&1``"
+else
+  echo "The current platform is Linux based"
+fi
+
+
+if [ -z "$ES_HOME" ] && [ -z "$KIBANA_HOME" ] ; then
+  echo "The path to Elasticsearch and Kibana are not available"
 else
   echo "Elasticsearch is configured at: $ES_HOME"
-  echo "Starting Elasticsearch service on $host"
-  cd $ES_HOME
-  ./bin/elasticsearch -d -p elasticsearch.pid > elasticsearch.log 2>&1``
+  echo "Kibana is configured at: $KIBANA_HOME"
+  echo "Saving data in $LOCAL_DIR"
+  eval $ELK_MODE
 fi
