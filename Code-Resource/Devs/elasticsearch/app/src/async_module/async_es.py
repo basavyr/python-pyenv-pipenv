af9fa17a378dee0269f5b7a1394038d8cb04afe1
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-04-10 08:56:54 +0300

    improve the async module

--- Code-Resource/Devs/elasticsearch/app/src/async_module/async_es.py
+++ Code-Resource/Devs/elasticsearch/app/src/async_module/async_es.py
@@ -1,15 +1,26 @@
-from elasticsearch import Elasticsearch, AsyncElasticsearch
+import json
+
 import asyncio
+from elasticsearch import Elasticsearch, AsyncElasticsearch
 
 from configs import config
 
 
-async def get_all_docs():
-    async_es = AsyncElasticsearch(
-        ['https://localhost:9200/'], verify_certs=True, ca_certs=config.CERT_PATH)
+def create_async_es_client() -> AsyncElasticsearch:
+    """
+    - creates an asynchronous Elasticsearch connection to an existing client
+    """
+    async_es_client = AsyncElasticsearch(
+        hosts='https://localhost:9200/', api_key=(config.ES_API_KEY['id'], config.ES_API_KEY['key']),
+        ca_certs=config.CERT_PATH)
 
-    response = await async_es.async_search(
-        index='gicu-test',
+    return async_es_client
+
+
+async def get_all_docs(es_client: AsyncElasticsearch, es_index: str) -> dict:
+
+    response = await es_client.async_search(
+        index=es_index,
         body={
             'query': {
                 'match_all': {}
@@ -17,16 +28,39 @@ async def get_all_docs():
         }
     )
 
-    await async_es.close()
+    # await es_client.close()
+    return response
 
-    return response['hits']['hits']
 
-es = Elasticsearch(
-    ['https://localhost:9200/'],
-    verify_certs=True, ca_certs=config.CERT_PATH)
+async def get_fixed_docs(es_client, n_docs):
+    resp = await es_client.search(
+        index="debug_index",
+        body={
+            "query":
+                {"match_all": {}}
+        },
+        size=n_docs,
+    )
+    print(resp)
 
-if es.indices.exists(index='gicu-test'):
-    docs = asyncio.run(get_all_docs())
-    print(docs)
-else:
-    print("Index 'gicu-test' does not exist")
+
+async def get_es_indices(es_client):
+    # Get Document API
+    # this is a test document, which already exists in the Elasticsearch index
+    test_id_debug = 'ax-XaYcB_ivX9fnUxzlV'
+    # the id was automatically generated when the POST command was executed from Kibana Dev Tools
+    id_1 = await es_client.get(index="debug_index", id=test_id_debug)
+    # print(id_1)
+
+    # Get Index API
+    test_index = "debug_index"
+    indices = await es_client.indices.get(index=test_index)
+
+    for k, v in indices.items():
+        if k == test_index:
+            print(v['mappings'])
+
+
+async def print_info(es_client):
+    info = await es_client.info()
+    print(info)
